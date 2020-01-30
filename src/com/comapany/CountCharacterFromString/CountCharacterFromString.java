8a0b864f36570eb7a9a65c543505f947ce7574c7
Shishir Roy <nandini12majumdar@gmail.com>
2020-01-30 15:09:12 +0530

    characterCount method add

---
+++ src/com/comapany/CountCharacterFromString/CountCharacterFromString.java
@@ -0,0 +1,27 @@
+package com.comapany.CountCharacterFromString;
+import java.util.HashMap;
+
+public class CountCharacterFromString {
+	public static void characterCount(String inputString)
+	{
+		HashMap<Character,Integer> charCountMap =new  HashMap<Character, Integer>();
+		char[] strArray=inputString.toCharArray();
+		for (char c : strArray) {
+			if(charCountMap.containsKey(c)) {
+				charCountMap.put(c,charCountMap.get(c)+1);
+			}
+			else
+			{
+				charCountMap.put(c,1);
+			}
+			
+		}
+		System.out.println(inputString+" : "+charCountMap);
+	}
+
+	public static void main(String[] args) {
+		characterCount("JavaJ2EE");
+
+	}
+
+}
